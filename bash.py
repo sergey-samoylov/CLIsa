152a4e96f1ea5edfecef821807b0bb4877c62f0d
Sergey Samoylov <linews@yandex.ru>
2023-04-17 08:48:52 +0300

    GIT3inONEscript Пн 17 апр 2023 08:48:52 MSK

--- bash.py
+++ bash.py
@@ -3,6 +3,9 @@ import os
 def about_comp():
     return os.system("df -h | grep a")
 
+def git3():
+    return os.system("cd $HOME/Dev/CLIsa && git4git.sh")
+
 def music():
     return os.system("music.sh")
 
